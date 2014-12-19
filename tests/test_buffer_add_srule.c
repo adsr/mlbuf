c58e8d414a2e99ff8495aa41e75a2fedf3c2dee1
Adam Saponara <saponara@gmail.com>
2014-12-18 23:32:44 -0500

    test_buffer_add_mark; test_buffer_add_srule

--- tests/test_buffer_add_srule.c
+++ tests/test_buffer_add_srule.c
@@ -1,6 +1,16 @@
 #include "test.h"
 
 MAIN("hello\nworld",
-    // TODO
+    size_t i;
+    srule_t* srule;
+    srule = srule_new_single("world", sizeof("world"), 1, 2);
+    buffer_add_srule(buf, srule);
+    for (i = 0; i < buf->first_line->char_count; i++) {
+        ASSERT("line1fg", 0, buf->first_line->char_styles[i].fg);
+        ASSERT("line1bg", 0, buf->first_line->char_styles[i].bg);
+    }
+    for (i = 0; i < buf->first_line->next->char_count; i++) {
+        ASSERT("line2fg", 1, buf->first_line->next->char_styles[i].fg);
+        ASSERT("line2bg", 2, buf->first_line->next->char_styles[i].bg);
+    }
 )
-

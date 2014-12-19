6e07b8761a2d8a2a44d98a7e9947303bb906d716
Adam Saponara <saponara@gmail.com>
2014-12-19 00:19:21 -0500

    test_buffer_set; test_buffer_substr; test_buffer_undo

--- tests/test_buffer_substr.c
+++ tests/test_buffer_substr.c
@@ -1,6 +1,13 @@
 #include "test.h"
 
 MAIN("hello\nworld",
-    // TODO
-)
+    char *data;
+    size_t data_len;
+    size_t nchars;
 
+    buffer_substr(buf, buf->first_line, 4, buf->first_line->next, 1, &data, &data_len, &nchars);
+    ASSERT("datalen", 3, data_len);
+    ASSERT("nchars", 3, nchars);
+    ASSERT("substr", 0, strncmp("o\nw", data, data_len));
+    free(data);
+)
